74a05b1e6af531004a1e1f09459a59cd72fb1a30
MANAN TANDON <manantandon@gmail.com>
2023-08-07 16:23:40 +0530

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import logo from "./Assets/logo two.png";
+import { useState, useEffect } from "react";
+
+export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isScrolled]);
+
+  return (
+    <header className={`z-[100]`}>
+      <div className="flex items-center space-x-2 md:space-x-2">
+        <Image
+          src={logo}
+          alt=""
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+      </div>
+      <div>
+        <ul className=" flex space-x-4">
+          <li className="headerLink">Class 11</li>
+          <li className="headerLink">Class 12</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <img
+          src="https://rb.gy/g1pwyx"
+          alt="user image"
+          className="cursor-pointer rounded-full"
+        />
+      </div>
+    </header>
+  );
+};
